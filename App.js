7c5468229383a213fa468da872398463b2bbd1dc
DavyHyun <dhyun1124@gmail.com>
2021-12-29 23:00:03 -0800

    remnoved 'git'

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is the Frosty App!git</Text>
+      <Text>This is the Frosty App!</Text>
       <StatusBar style="auto" />
     </View>
   );
