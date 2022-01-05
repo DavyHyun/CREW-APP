5df1cfb101e4cd2b4c48472b1bf9d190d336f6bb
Collin Kim <collinkim@Collins-MBP.hsd1.wa.comcast.net>
2022-01-05 09:19:06 -0800

    HomeScreen Update

---
+++ Screens/FriendsHome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+const FriendsHome = () => {
+    return (
+        <View>
+            <Text>FriendsHome</Text>
+        </View>
+    )
+}
+
+export default FriendsHome
