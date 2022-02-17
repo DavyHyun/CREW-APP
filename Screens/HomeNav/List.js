a433cd3db538740b9375be8e18bacce12ef8956d
cokim5 <collinkim.5@hotmail.com>
2022-02-16 20:02:07 -0800

    fixed minor bug by adding try catch

--- Screens/HomeNav/List.js
+++ Screens/HomeNav/List.js
@@ -6,15 +6,16 @@ import { auth } from '../../firebase'
 import { getDatabase, ref, set, get, child, onValue } from "firebase/database";
 import firebase from "firebase/compat/app";
 import { getAuth } from "firebase/auth";
-import { SimpleLineIcons } from '@expo/vector-icons'; 
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 
 const List = ({ route }) => {
   const navigation = useNavigation();
   const [visible, setVisible] = useState(false);
   const [name, setName] = useState("");
-  const score = route.params.score;
-  
+  const [score, setScore] = useState(0);
+
+
 
   useEffect(() => {
     try {
@@ -24,25 +25,33 @@ const List = ({ route }) => {
       onValue(nameRef, (snapshot) => {
         const data = snapshot.val();
         setName(data.name)
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}, [])
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const obj = route.params.score;
+      console.log(obj);
+      console.log("useeffect has been run");
+    } catch (error) {
+      console.log(error);
+      console.log("useeffect has been run");
+    }
+  }, [])
 
   return (
-      <View style={styles.container}>
-        <View style={styles.Top}>
+    <View style={styles.container}>
+      <View style={styles.Top}>
         <TouchableOpacity
-                onPress={()=> navigation.openDrawer()}
-                // style={styles.backButton}
-            >
-                <SimpleLineIcons name="menu" size={24} color="black" />
-            </TouchableOpacity>
-            <Text>Your List</Text>
-            <Text>{score}</Text>
-        </View>
+          onPress={() => navigation.openDrawer()}
+        // style={styles.backButton}
+        >
+          <SimpleLineIcons name="menu" size={24} color="black" />
+        </TouchableOpacity>
+        <Text>Your List</Text>
+        <Text>{score}</Text>
       </View>
+    </View>
   )
 }
 
@@ -50,22 +59,22 @@ export default List
 
 const styles = StyleSheet.create({
 
-container: {
+  container: {
     justifyContent: 'center',
     alignItems: 'center',
     // backgroundColor:'black'
-},
-  
+  },
 
-Top: {
-// height: '50%',
+
+  Top: {
+    // height: '50%',
     width: '80%',
     marginTop: '15%',
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
     // backgroundColor:'blue',
-},
+  },
 
 })
 
