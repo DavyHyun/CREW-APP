54f7a4a382688c9fe75f6ba61b7e345f34b0cf60
cokim5 <collinkim.5@hotmail.com>
2022-05-02 03:52:41 -0700

    pretty sure nothing changed from last commit but also idk brain don't be working anymore

--- Screens/Multiplayer/WaitingRoom.js
+++ Screens/Multiplayer/WaitingRoom.js
@@ -8,21 +8,34 @@ import AppLoading from 'expo-app-loading';
 // import { FlatList } from 'react-native-gesture-handler';
 import { render } from 'react-dom';
 
+const Item = ({ personName }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{personName}</Text>
+    </View>
+  );
+
 const WaitingRoom = () => {
     const navigation = useNavigation();
     const route = useRoute();
     const roomID = route.params;
-    const [playerList, setPlayerList] = useState([]);
+    const playerList = [];
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+      );
+    let players;
 
     useEffect(() => {
         try {
             const db = getDatabase();
             const usersRef = ref(db, 'lobby/' + roomID + '/users');
             onValue(usersRef, (snapshot) => {
-                const players = snapshot.toJSON();
+                players = snapshot.toJSON();
                 const length = Object.keys(players).length;
+                console.log(players);
+                console.log("break");
                 for (let index = 0; index < length; index++) {
-                    console.log(players[index].personName);             
+                    console.log(players[index].personName);
+                    playerList.push(players[index].personName);             
                 }
                 
             })
