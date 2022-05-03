1e50bb2298037e7e629578589559de7c0e094c59
cokim5 <collinkim.5@hotmail.com>
2022-05-02 23:00:46 -0700

    hope this shit merges

---
+++ Screens/Multiplayer/MultiplayerQuiz/DisplayBuffer.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View, TouchableOpacity, Touchable, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, useRoute, useIsFocused } from '@react-navigation/core';
+import { getStorage, getDownloadURL, ref as sRef } from "firebase/storage";
+import rData from '../../json/thankYouGrace.json';
+import AppLoading from 'expo-app-loading';
+import { Svg } from 'expo';
+import { getDatabase, ref, set, get, child, onValue, push } from "firebase/database";
