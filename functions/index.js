82923600f96dc63387323a58700c2a268223b078
DavyHyun <dhyun1124@gmail.com>
2022-05-20 01:00:42 -0700

    Final V 1.5 I hope

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { query } = require("@firebase/firestore");
+// const { query } = require("@firebase/firestore");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,36 +10,6 @@ const { query } = require("@firebase/firestore");
 //   response.send("Hello from Firebase!");
 // });
 // alias firebase="`npm config get prefix`/bin/firebase"
-
-exports.singlePlayerText = functions.https.onCall((data, context) => {
-  //GETTING EVERYTHING WHERE MOOD IS FALSE
-  return new Promise(function (resolve, reject) {
-    var db = admin.firestore();
-    var result = [];
-    const resRef = db.collection('restaurants');
-    resRef.where("MOOD", "==", false).get().then((querySnpshot) => {
-      querySnpshot.forEach(doc => {
-        result.push({
-          resName: doc.data().RESTAURANT,
-        })
-      })
-      console.log(result);
-      resolve(result);
-    })
-
-
-    // resRef.get().then((querySnapshot) => {
-    //   querySnapshot.forEach(doc => {
-    //     console.log(doc.data().RESTAURANT);
-    //     if (doc.data().RESTAURANT === 'Timeless Tea') {
-    //       console.log("FOUND IT")
-    //       result = doc.data().RESTAURANT;
-    //       resolve(result);
-    //     }
-    //   });
-    //  });
-  })
-});
 
 exports.loadNationalities = functions.https.onCall((data, context) => {
   return new Promise(function (resolve, reject) {
